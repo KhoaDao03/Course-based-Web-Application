93a590d6c232bac847721a8b83b4686d12396c13
Khoa Dang Dao <125584971+KhoaDao03@users.noreply.github.com>
2024-01-21 23:38:59 -0500

    Initial Commit

---
+++ App.LearningManagement/Library.LearningManagement/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.LearningManagement.Models
+{
+    public class Person
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public Dictionary<int, double> Grades { get; set; }
+        public char Classification { get; set; }
+        public Person()
+        {
+            Grades = new Dictionary<int, double>();
+        }
+    }
+}
