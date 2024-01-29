6cc3bca890d773db7e95bbabe21a61430f3a9388
Khoa Dang Dao <kdd21@fsu.edu>
2024-01-29 16:33:06 -0500

    FromAsus

--- App.LearningManagement/App.LearningManagement/obj/Debug/net8.0/App.LearningManagement.AssemblyInfo.cs
+++ App.LearningManagement/App.LearningManagement/obj/Debug/net8.0/App.LearningManagement.AssemblyInfo.cs
@@ -14,8 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.LearningManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4250b72603a2966895dfeb65c5495856ad485d27")]
-
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92efa70e64c5bda052b8fcab20ba72655328fe61")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.LearningManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.LearningManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
